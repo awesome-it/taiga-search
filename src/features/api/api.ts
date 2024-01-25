61bcd2423463b88461295924fa471f195fb02842
Sven Hoth <sven.hoth@awesome-it.de>
2024-01-25 16:47:31 +0100

    Add login

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -1,6 +1,23 @@
 import Keycloak from 'keycloak-js'
 import {Issue, Project, SearchResults, Task, User, UserStory} from '../../types/taiga.ts'
 
+async function login(keycloakToken: string) {
+  const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      type: 'openid',
+      code: keycloakToken,
+      url: document.location.href,
+    }),
+  })
+  if (!response.ok) {
+    const result = await response.json()
+    throw new Error(result.message)
+  }
+  return response.json()
+}
 const keycloak = new Keycloak({
   url: import.meta.env.VITE_KEYCLOAK_URL,
   realm: import.meta.env.VITE_KEYCLOAK_REALM,
@@ -12,6 +29,8 @@ keycloak
   })
   .then(authenticated => {
     console.log('Keycloak login', {authenticated, keycloak})
+    const result = login(keycloak.token!)
+    console.log({result})
   })
   .catch(error => {
     console.warn('Keycloak init', {error})
