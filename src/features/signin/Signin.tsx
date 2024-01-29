b59fc68917e10675a091ef18c80ffdcbf29fd385
Sven Hoth <sven.hoth@awesome-it.de>
2024-01-29 17:03:36 +0100

    Move login function

--- src/features/signin/Signin.tsx
+++ src/features/signin/Signin.tsx
@@ -13,6 +13,43 @@ function Signin() {
     }
   }, [auth, hasTriedSignin])
 
+  useEffect(() => {
+    const login = async (keycloakToken: string) => {
+      const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          type: 'openid',
+          code: keycloakToken,
+          url: document.location.href,
+        }),
+      })
+      if (!response.ok) {
+        const result = await response.json()
+        throw new Error(result.message)
+      }
+      return response.json()
+    }
+    const searchParams = new URLSearchParams(window.location.search)
+    const code = searchParams.get('code')
+
+    if (code) {
+      login(code)
+        .then(result => {
+          console.log('Result', {result})
+        })
+        .catch(error => {
+          console.log('Error', {error})
+        })
+      // TODO: when ready re-add finally
+      // .finally(() => {
+      //   window.history.replaceState({}, document.title, window.location.pathname)
+      // })
+    }
+  }, [])
+
   if (auth.isLoading) {
     return <div>Loading...</div>
   }
