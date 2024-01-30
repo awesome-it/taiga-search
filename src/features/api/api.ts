e4becbaa10116b250528d3f5d8f6aa4552618068
Sven Hoth <sven.hoth@awesome-it.de>
2024-01-30 16:09:58 +0100

    Implement custom UserManager to enable taiga login against keycloak

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -1,50 +1,11 @@
-// import Keycloak from 'keycloak-js'
 import {Issue, Project, SearchResults, Task, User, UserStory} from '../../types/taiga.ts'
 
-// async function login(keycloakToken: string) {
-//   const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
-//     headers: {
-//       'Content-type': 'application/json',
-//     },
-//     method: 'POST',
-//     body: JSON.stringify({
-//       type: 'openid',
-//       code: keycloakToken,
-//       url: document.location.href,
-//     }),
-//   })
-//   if (!response.ok) {
-//     const result = await response.json()
-//     throw new Error(result.message)
-//   }
-//   return response.json()
-// }
-
-// const keycloak = new Keycloak({
-//   url: import.meta.env.VITE_KEYCLOAK_URL,
-//   realm: import.meta.env.VITE_KEYCLOAK_REALM,
-//   clientId: import.meta.env.VITE_KEYCLOAK_CLIENTID,
-// })
-// keycloak
-//   .init({
-//     onLoad: 'login-required',
-//     enableLogging: true,
-//     // flow: 'hybrid',
-//     // scope: 'openid email',
-//   })
-//   .then(async authenticated => {
-//     console.log('Keycloak login', {authenticated, keycloak})
-//     const result = await login(keycloak.token!)
-//     console.log({result})
-//   })
-//   .catch(error => {
-//     console.warn('Keycloak init', {error})
-//   })
-
 async function fetchWithPath(path: string) {
+  const token = window.localStorage.getItem('auth-token')
+
   const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}${path}`, {
     headers: {
-      Authorization: `Bearer ${window.localStorage.getItem('auth-token')}`,
+      Authorization: `Bearer ${token}`,
     },
   })
   if (!response.ok) {
--- src/features/signin/Signin.tsx
+++ src/features/signin/Signin.tsx
@@ -13,49 +13,6 @@ function Signin() {
     }
   }, [auth, hasTriedSignin])
 
-  useEffect(() => {
-    const login = async (keycloakToken: string) => {
-      const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
-        headers: {
-          'Content-type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-          type: 'openid',
-          code: keycloakToken,
-          url: document.location.origin + document.location.pathname,
-        }),
-      })
-      if (!response.ok) {
-        const result = await response.json()
-        throw new Error(result.message)
-      }
-      return response.json()
-    }
-    const searchParams = new URLSearchParams(window.location.search)
-    const code = searchParams.get('code')
-
-    if (code) {
-      login(code)
-        .then(result => {
-          console.log('Result', {result})
-          console.log('User', auth.user)
-          window.localStorage.setItem('auth-token', result.auth_token)
-          window.localStorage.setItem('refresh-token', result.refresh)
-          auth.isAuthenticated = true
-        })
-        .catch(error => {
-          console.log('Error', {error})
-        })
-        .finally(() => {
-          window.history.replaceState({}, document.title, window.location.pathname)
-        })
-    }
-  }, [auth])
-
-  console.log('Auth', {auth})
-  console.log('Authenticated', auth.isAuthenticated)
-
   if (auth.isLoading) {
     return <div>Loading...</div>
   }
