3c3127e07b0cee0cd76fe85a11507d280fc6e5e6
Sven Hoth <sven.hoth@awesome-it.de>
2024-01-30 16:09:58 +0100

    Implement custom UserManager to enable taiga login against keycloak

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,49 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import {AuthProvider} from 'react-oidc-context'
-import {WebStorageStateStore} from 'oidc-client-ts'
-// import App from './App.tsx'
+import {User, UserManager, WebStorageStateStore} from 'oidc-client-ts'
 import Signin from './features/signin/Signin.tsx'
+
+class TaigaUserManager extends UserManager {
+  protected async _signinEnd(url: string, verifySub?: string): Promise<User> {
+    const logger = this._logger.create('_signinEnd')
+    const login = async (keycloakToken: string) => {
+      const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          type: 'openid',
+          code: keycloakToken,
+          url: document.location.origin + document.location.pathname,
+        }),
+      })
+      if (!response.ok) {
+        const result = await response.json()
+        throw new Error(result.message)
+      }
+      return response.json()
+    }
+
+    const searchParams = new URLSearchParams(window.location.search)
+    const code = searchParams.get('code')
+    const {response} = await this._client.readSigninResponseState(url, true)
+
+    if (code) {
+      try {
+        const result = await login(code)
+        response.access_token = result.auth_token
+        response.refresh_token = result.refresh
+        window.localStorage.setItem('auth-token', response.access_token)
+      } catch (e) {
+        logger.debug('Error on taiga login', e)
+      }
+    }
+
+    return this._buildUser(response, verifySub)
+  }
+}
 
 const router = createBrowserRouter(
   [
@@ -25,39 +65,11 @@ const router = createBrowserRouter(
   },
 )
 const queryClient = new QueryClient()
-async function login(keycloakToken: string) {
-  const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
-    headers: {
-      'Content-type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      type: 'openid',
-      code: keycloakToken,
-      url: document.location.href,
-    }),
-  })
-  if (!response.ok) {
-    const result = await response.json()
-    throw new Error(result.message)
-  }
-  return response.json()
-}
 
 const userStoreProp = new WebStorageStateStore({store: window.localStorage})
-const signinCallback = async (): Promise<void> => {
-  const searchParams = new URLSearchParams(window.location.search)
-  const code = searchParams.get('code')!
 
+const onSigninCallback = (): void => {
   window.history.replaceState({}, document.title, window.location.pathname)
-  await login(code)
-    .then(result => {
-      console.log('Result', {result})
-      userStoreProp.set('auth-token', result.auth_token)
-    })
-    .catch(error => {
-      console.log('Error', {error})
-    })
 }
 
 createRoot(document.getElementById('root')!).render(
@@ -66,11 +78,11 @@ createRoot(document.getElementById('root')!).render(
       authority={`${import.meta.env.VITE_KEYCLOAK_URL}/realms/${import.meta.env.VITE_KEYCLOAK_REALM}`}
       client_id={import.meta.env.VITE_KEYCLOAK_CLIENTID}
       redirect_uri={window.location.origin + window.location.pathname}
-      onSigninCallback={signinCallback}
       userStore={userStoreProp}
       scope="openid email"
+      implementation={TaigaUserManager}
       disablePKCE
-      skipSigninCallback
+      onSigninCallback={onSigninCallback}
     >
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
