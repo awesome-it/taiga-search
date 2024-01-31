b6c276518d2e33ca297b24323711517b893ac6b1
Sven Hoth <sven.hoth@awesome-it.de>
2024-01-31 09:19:41 +0100

    Implement first fixes

--- src/features/search/SearchResults.tsx
+++ src/features/search/SearchResults.tsx
@@ -1,11 +1,22 @@
-import {Divider, List, ListItem, ListItemButton, ListItemText} from '@mui/material'
+import {Divider, Link, List, ListItem, ListItemButton, ListItemText, Typography} from '@mui/material'
 import {useQueryClient} from '@tanstack/react-query'
+import {useParams} from 'react-router-dom'
 import {SearchResult, SearchResultMap} from '../../types/search.ts'
 import {Project} from '../../types/taiga.ts'
 
 function SearchResults({results}: {results: SearchResult}) {
+  const {searchTerm} = useParams()
   const queryClient = useQueryClient()
   const projects: Project[] | undefined = queryClient.getQueryData(['projects'])
+
+  if ((Object.keys(results) as Array<keyof SearchResult>).every(key => results[key].length === 0)) {
+    return (
+      <Typography>
+        Sorry! There are no results for &quot;{searchTerm}&quot; <br />
+        <Link href="/">Return to dashboard</Link>
+      </Typography>
+    )
+  }
   return (
     <List>
       {(Object.keys(results) as Array<keyof SearchResult>).map(key =>
