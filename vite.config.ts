5afda751871cd1ca20b746df74341e19aa67fc57
Sven Hoth <sven.hoth@awesome-it.de>
2024-02-12 17:38:32 +0100

    Revert "Task-699: Try new live deployment"
    This reverts commit b4ff9dde059b31001632f8eb2491127c0fc5afb2.

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,15 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => {
-  if (command === 'serve') {
-    return {
-      base: '/search',
-      server: {
-        host: true,
-        port: 5173,
-      },
-    }
-  }
-  return {
-    plugins: [react()],
-    base: '/',
-    server: {
-      host: true,
-      port: 5173,
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/search',
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+  },
 })
