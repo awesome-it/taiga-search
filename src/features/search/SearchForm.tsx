d0512f004b82f87eba068fbcbb998ef39e48b416
Sven Hoth <sven.hoth@awesome-it.de>
2024-02-12 11:34:00 +0100

    Task-696: Fix powersearch

--- src/features/search/SearchForm.tsx
+++ src/features/search/SearchForm.tsx
@@ -3,56 +3,19 @@ import {ChangeEvent, FormEventHandler, useCallback, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import ClearIcon from '@mui/icons-material/Clear'
 import SearchIcon from '@mui/icons-material/Search'
-import useTaigaQueries from '../queries/queries.ts'
 import {useFiltersDispatch} from '../filter/FilterProvider.tsx'
 
 function SearchForm({searchTerm = ''}: {searchTerm: string}) {
   const [searchInput, setSearchInput] = useState(searchTerm)
   const navigate = useNavigate()
-  const taigaQueries = useTaigaQueries()
-  const {data: allProjects} = taigaQueries.useProjectQuery()
   const setFilters = useFiltersDispatch()
-
-  const parseFilters = useCallback(
-    (searchTermToParse: string) => {
-      const re = /project:(\w+)/g
-      const result = re.exec(searchTermToParse)
-      if (result) {
-        // Powersearch that filters for a project
-        const search = searchTermToParse.replace(result[0], '').trim()
-
-        if (allProjects) {
-          const project = allProjects.find(pro => {
-            return result[1].trim().toLowerCase() === pro.name.toLowerCase()
-          })
-          if (project) {
-            setFilters(filters => {
-              const newProjects =
-                !filters.projects || filters.projects.length === 0
-                  ? [project!.id]
-                  : filters.projects && filters.projects.length > 0 && !filters.projects.includes(project!.id)
-                    ? [...filters.projects, project!.id]
-                    : filters.projects
-
-              return {...filters, projects: newProjects}
-            })
-          }
-          return search.trim()
-        }
-      }
-      return searchTermToParse
-    },
-    [setFilters, allProjects],
-  )
 
   const onSubmitHandler: FormEventHandler<HTMLFormElement> = useCallback(
     e => {
       e.preventDefault()
-      const parsedSearch = parseFilters(searchInput)
-      setSearchInput(parsedSearch)
-      navigate(`/${parsedSearch}`)
+      navigate(`/${searchInput}`)
     },
-    [navigate, searchInput, parseFilters],
+    [navigate, searchInput],
   )
 
   const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value), [])
--- src/features/widgets/TicketWidget.tsx
+++ src/features/widgets/TicketWidget.tsx
@@ -1,8 +1,10 @@
 import {Card, CardContent, CardHeader} from '@mui/material'
 import {useCallback} from 'react'
+import {useParams} from 'react-router-dom'
 import {Issue, Project, Task, UserStory} from '../../types/taiga.ts'
 import TicketList from './TicketList.tsx'
 import {useFilters} from '../filter/FilterProvider.tsx'
+import useTaigaQueries from '../queries/queries.ts'
 
 function TicketWidget({
   tickets,
@@ -14,18 +16,47 @@ function TicketWidget({
   style?: object
 }) {
   const filters = useFilters()
+  const taigaQueries = useTaigaQueries()
+  const {data: allProjects} = taigaQueries.useProjectQuery()
 
   const filteredTickets = useCallback(() => {
+    const {searchTerm} = useParams()
+    let tempFilters = {} as {projects: number[]}
+
+    if (searchTerm) {
+      const re = /project:(\w+)/g
+      const result = re.exec(searchTerm)
+      if (result) {
+        // Powersearch that filters for a project
+        if (allProjects) {
+          const project = allProjects.find(pro => {
+            return result[1].trim().toLowerCase() === pro.name.toLowerCase()
+          })
+          if (project) {
+            tempFilters = {...tempFilters, projects: [project!.id]}
+          }
+        }
+      }
+    }
     return tickets.filter(ticket => {
+      let isFiltered = true
       if (filters && filters.projects && filters.projects.length > 0) {
         if (ticket.isProject) {
-          return filters.projects.includes(ticket.id)
+          isFiltered = filters.projects.includes(ticket.id)
+        } else {
+          isFiltered = filters.projects.includes(ticket.project)
         }
-        return filters.projects.includes(ticket.project)
       }
-      return true
+      if (tempFilters && tempFilters.projects && tempFilters.projects.length > 0) {
+        if (ticket.isProject) {
+          isFiltered = tempFilters.projects.includes(ticket.id)
+        } else {
+          isFiltered = tempFilters.projects.includes(ticket.project)
+        }
+      }
+      return isFiltered
     })
-  }, [filters, tickets])
+  }, [allProjects, filters, tickets])
   return (
     <Card variant="outlined">
       {title && (
