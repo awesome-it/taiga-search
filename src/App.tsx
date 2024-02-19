b16a923df6457e74e400c657be231d33dd32424a
Daniel Morlock <daniel.morlock@awesome-it.de>
2024-02-19 15:26:00 +0100

    Re-do old behavior on small devices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useParams} from 'react-router-dom'
 import {Box} from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
 import SearchForm from './features/search/SearchForm.tsx'
 import SearchResults from './features/search/SearchResults.tsx'
 import ProjectFilter from './features/filter/ProjectFilter.tsx'
@@ -13,14 +14,14 @@ function App() {
   return (
     <FilterProvider>
       <Box display="flex" flexDirection="column" gap={2} flexGrow={1} m={2} sx={{minHeight: '0px'}}>
-        <Box display="flex" flexDirection="row" gap={2}>
-          <Box flexGrow={3}>
+        <Grid container spacing={2}>
+          <Grid xs={12} sm={7} md={8}>
             <SearchForm searchTerm={searchTerm} />
-          </Box>
-          <Box flexGrow={1}>
+          </Grid>
+          <Grid xs={12} sm={5} md={4}>
             <ProjectFilter />
-          </Box>
-        </Box>
+          </Grid>
+        </Grid>
         {searchTerm ? <SearchResults searchTerm={searchTerm} /> : <DashboardView />}
       </Box>
     </FilterProvider>
