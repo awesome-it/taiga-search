2151fbd1c6fd6d6e4bc3aebc9252cf806f8d70d3
Sven Hoth <sven.hoth@awesome-it.de>
2024-04-04 08:51:41 +0200

    TG-741: Add api-auth as configurable alternative login

---
+++ src/features/signin/ApiSignIn.tsx
@@ -0,0 +1,79 @@
+import {Alert, Box, Button, CircularProgress, Paper, TextField, Typography} from '@mui/material'
+import {FormEventHandler, useState} from 'react'
+import {User} from '../../types/taiga.ts'
+import useAuth from '../auth/authWrapper.ts'
+import App from '../../App.tsx'
+
+export function ApiSignIn() {
+  const {token} = useAuth()
+  const [loginError, setLoginError] = useState<string | null>(null)
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const login = async (username: string, pass: string): Promise<User> => {
+    const response = await fetch(`${import.meta.env.VITE_TAIGA_BASE_URL}/auth`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password: pass,
+        type: 'normal',
+      }),
+    })
+    if (!response.ok) {
+      const result = await response.json()
+      throw new Error(result.detail)
+    }
+    return response.json()
+  }
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = form => {
+    form.preventDefault()
+    const formData = new FormData(form.currentTarget)
+    const username = formData.get('username') as string
+    const password = formData.get('password') as string
+    setLoading(true)
+    login(username, password)
+      .then(user => {
+        window.localStorage.setItem('token', JSON.stringify(user.auth_token))
+        window.localStorage.setItem('refresh', JSON.stringify(user.refresh))
+        window.location.reload()
+      })
+      .catch(e => {
+        setLoginError(e.message)
+        setLoading(false)
+      })
+  }
+
+  return token ? (
+    <App />
+  ) : (
+    <Box
+      component="form"
+      onSubmit={submitHandler}
+      display="flex"
+      flexDirection="column"
+      flexGrow={1}
+      sx={{minHeight: '0px'}}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Paper elevation={10} sx={{display: 'flex', flexDirection: 'column', gap: 2, p: 5}}>
+        <Typography variant="h4" component="h1">
+          Login to Taiga
+        </Typography>
+        {loginError && <Alert color="error">{loginError}</Alert>}
+        <TextField name="username" label="Username" />
+        <TextField name="password" label="Password" type="password" />
+        <Button
+          type="submit"
+          disabled={isLoading}
+          endIcon={isLoading ? <CircularProgress /> : null}
+          variant="contained"
+        >
+          Sign In
+        </Button>
+      </Paper>
+    </Box>
+  )
+}
