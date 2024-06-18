77f40e24a565d20beb41bdeb6f662344073a3622
Daniel Morlock <daniel.morlock@awesome-it.de>
2024-06-18 09:03:11 +0200

    Remove custom styles, use native dialog components.

--- src/features/signin/ApiSignIn.tsx
+++ src/features/signin/ApiSignIn.tsx
@@ -1,6 +1,6 @@
 import {FormEventHandler, useState} from 'react'
 import Alert from '@mui/material/Alert'
-import {Box, Dialog, DialogContent, TextField, Typography} from '@mui/material'
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from '@mui/material'
 import {LoadingButton} from '@mui/lab'
 import {User} from '../../types/taiga.ts'
 import useAuth from '../auth/authWrapper.ts'
@@ -29,7 +29,7 @@ export function ApiSignIn() {
     return response.json()
   }
 
-  const submitHandler: FormEventHandler<HTMLFormElement> = form => {
+  const onSubmit: FormEventHandler<HTMLFormElement> = form => {
     form.preventDefault()
     const formData = new FormData(form.currentTarget)
     const username = formData.get('username') as string
@@ -50,30 +50,25 @@ export function ApiSignIn() {
   return token ? (
     <App />
   ) : (
-    <Dialog open={!token} fullWidth maxWidth="sm">
+    <Dialog open={!token} fullWidth maxWidth="sm" PaperProps={{component: 'form', onSubmit}}>
       <DialogContent>
-        <Box
-          component="form"
-          onSubmit={submitHandler}
-          display="flex"
-          flexDirection="column"
-          flexGrow={1}
-          gap={2}
-          p={5}
-          sx={{minHeight: '0px'}}
-          alignItems="stretch"
-          justifyContent="center"
-        >
-          <Typography align="center" variant="h4" component="h1">
-            Login to Taiga
-          </Typography>
-          {loginError && <Alert color="error">{loginError}</Alert>}
-          <TextField name="username" label="Username" />
-          <TextField name="password" label="Password" type="password" />
-          <LoadingButton type="submit" fullWidth loading={isLoading} variant="contained">
+        <DialogTitle mb={2}>Taiga Login</DialogTitle>
+        <DialogContent>
+          {loginError && (
+            <Alert color="error" sx={{mb: 3}}>
+              {loginError}
+            </Alert>
+          )}
+          <DialogContentText display="flex" flexDirection="column" gap={2}>
+            <TextField name="username" label="Username" />
+            <TextField name="password" label="Password" type="password" />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <LoadingButton type="submit" loading={isLoading} variant="contained">
             Sign In
           </LoadingButton>
-        </Box>
+        </DialogActions>
       </DialogContent>
     </Dialog>
   )
